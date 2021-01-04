7bfcd67a6d193a10a756b35a57439b3a42852126
Sohanur Rahman <sohanur064@gmail.com>
2021-01-04 08:16:08 +0600

    Component Tree

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import MainComponent from './components/MainComponent';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <MainComponent />
     </div>
   );
 }
---
+++ src/components/MainComponent.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './header/Header';
+import Footer from './footer/Footer';
+import Body from './body/Body';
+
+
+
+const MainComponent = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default MainComponent
