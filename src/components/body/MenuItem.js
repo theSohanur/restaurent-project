8430835ce8d99a942cc071284df00e0134546b7e
Sohanur Rahman <sohanur064@gmail.com>
2021-01-04 09:24:51 +0600

    Add Menu Item

--- src/components/body/MenuItem.js
+++ src/components/body/MenuItem.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from 'react';
+import {Card, CardImg, CardImgOverlay, CardBody, CardTitle } from 'reactstrap';
 
 const MenuItem = props => {
     console.log(props);
     return (
         <div>
-            
+            <Card>
+                <CardBody>
+                    <CardImg width="50%" alt ={props.dish.name} src = {props.dish.image} style={{opacity:0.7}} ></CardImg>
+                    <CardImgOverlay>
+                        <CardTitle>{props.dish.name}</CardTitle>
+                    </CardImgOverlay>
+                </CardBody>
+            </Card>
         </div>
     )
 }
