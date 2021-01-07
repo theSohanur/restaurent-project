b038aa569165998fa8f02281fa559ae440780fa0
Sohanur Rahman <sohanur064@gmail.com>
2021-01-07 12:58:58 +0600

    Add class component Nav & import react router

--- src/components/header/Navigation.js
+++ src/components/header/Navigation.js
@@ -1,4 +1,4 @@
-import React,{ useState } from 'react';
+import React,{ Component, useState } from 'react';
 import {
     Collapse,
     Navbar,
@@ -14,50 +14,96 @@ import {
     NavbarText
   } from 'reactstrap';
 
-const Navigation = () => {
+  import { Link } from 'react-router-dom';
 
-    const [isOpen, setIsOpen] = useState(false);
-    const toggle = () => setIsOpen(!isOpen);
-    return (
-        <div>
-            <Navbar dark color='dark' >
-                <div className="container">
-                    <NavbarBrand href='/'>Restaurent</NavbarBrand>
-                    <NavbarToggler onClick={toggle} />
-                    <Collapse isOpen={isOpen} navbar>
-                    <Nav className="mr-auto" navbar>
-                        <NavItem>
-                        <NavLink href="/components/">Components</NavLink>
-                        </NavItem>
-                        <NavItem>
-                        <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-                        </NavItem>
-                        <UncontrolledDropdown nav inNavbar>
-                        <DropdownToggle nav caret>
-                            Options
-                        </DropdownToggle>
-                        <DropdownMenu right>
-                            <DropdownItem>
-                            Option 1
-                            </DropdownItem>
-                            <DropdownItem>
-                            Option 2
-                            </DropdownItem>
-                            <DropdownItem divider />
-                            <DropdownItem>
-                            Reset
-                            </DropdownItem>
-                        </DropdownMenu>
-                        </UncontrolledDropdown>
-                    </Nav>
-                    <NavbarText>Simple Text</NavbarText>
-                    </Collapse>
-                </div>
+
+
+  class Navigation extends Component{
+      constructor(props){
+          super(props);
+          this.state = {
+              isNavOpen: false
+          }
+      }
+      navToggle = () => {
+          this.setState({
+              isNavOpen:!this.state.isNavOpen
+          })
+      }
+      render(){
+          return(
+              <div>
+                  <Navbar dark color="dark" expand='sm'>
+                    <div className="container">
+                        <NavbarBrand href="/">Restaurent</NavbarBrand>
+                        <NavbarToggler onClick={this.navToggle} />
+                        <Collapse isOpen={this.state.isNavOpen} navbar>
+                            <Nav className='mr-auto' navbar>
+                                <NavItem>
+                                    <Link to='/' className="nav-link active">Home</Link>
+                                </NavItem>
+                                <NavItem>
+                                    <Link to='/menu' className="nav-link">Menu</Link>
+                                </NavItem>
+                                <NavItem>
+                                    <Link to='/about' className="nav-link ">About</Link>
+                                </NavItem>
+                                <NavItem>
+                                    <Link to='/contact' className="nav-link ">Contact</Link>
+                                </NavItem>
+                            </Nav>
+                        </Collapse>                       
+                    </div>
+                  </Navbar>
+              </div>
+          )
+      }
+  }
+
+// const Navigation = () => {
+
+//     const [isOpen, setIsOpen] = useState(false);
+//     const toggle = () => setIsOpen(!isOpen);
+//     return (
+//         <div>
+//             <Navbar dark color='dark' >
+//                 <div className="container">
+//                     <NavbarBrand href='/'>Restaurent</NavbarBrand>
+//                     <NavbarToggler onClick={toggle} />
+//                     <Collapse isOpen={isOpen} navbar>
+//                     <Nav className="mr-auto" navbar>
+//                         <NavItem>
+//                         <NavLink href="/components/">Components</NavLink>
+//                         </NavItem>
+//                         <NavItem>
+//                         <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+//                         </NavItem>
+//                         <UncontrolledDropdown nav inNavbar>
+//                         <DropdownToggle nav caret>
+//                             Options
+//                         </DropdownToggle>
+//                         <DropdownMenu right>
+//                             <DropdownItem>
+//                             Option 1
+//                             </DropdownItem>
+//                             <DropdownItem>
+//                             Option 2
+//                             </DropdownItem>
+//                             <DropdownItem divider />
+//                             <DropdownItem>
+//                             Reset
+//                             </DropdownItem>
+//                         </DropdownMenu>
+//                         </UncontrolledDropdown>
+//                     </Nav>
+//                     <NavbarText>Simple Text</NavbarText>
+//                     </Collapse>
+//                 </div>
                 
-            </Navbar>
-        </div>
-    )
-}
+//             </Navbar>
+//         </div>
+//     )
+// }
 
 export default Navigation
 
