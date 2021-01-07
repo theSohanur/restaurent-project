9a745b4be7e1cf335d9c3d97994d809d529bda7b
Sohanur Rahman <sohanur064@gmail.com>
2021-01-07 14:19:56 +0600

    add Some data

--- src/data/Dishes.js
+++ src/data/Dishes.js
@@ -182,6 +182,236 @@ const DISHES = [
                 date: "2019-12-02T17:57:28.556094Z"
             }
         ]
+    },
+    {
+        id: 4,
+        name: 'Chocolate Cake',
+        image: 'assets/images/4cake.jpg',
+        category: 'dessert',
+        label: '',
+        price: '170',
+        description: 'A delectable, semi-sweet New York Style Cheese Cake, with cracker crust and spiced with Indian cardamoms',
+        comments: [
+            {
+                id: 0,
+                rating: 5,
+                comment: "Imagine all the eatables, living in conFusion!",
+                author: "Shanto",
+                date: "2018-10-16T17:57:28.556094Z"
+            },
+            {
+                id: 1,
+                rating: 4,
+                comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+                author: "Karim",
+                date: "2017-09-05T17:57:28.556094Z"
+            },
+            {
+                id: 2,
+                rating: 3,
+                comment: "Eat it, just eat it!",
+                author: "Moinul",
+                date: "2019-02-13T17:57:28.556094Z"
+            },
+            {
+                id: 3,
+                rating: 4,
+                comment: "Ultimate, Reaching for the stars!",
+                author: "Rahat",
+                date: "2017-12-02T17:57:28.556094Z"
+            },
+            {
+                id: 4,
+                rating: 2,
+                comment: "It's your birthday, we're gonna party!",
+                author: "Rony",
+                date: "2019-12-02T17:57:28.556094Z"
+            }
+        ]
+    },
+    {
+        id: 5,
+        name: 'Horlicks Cake',
+        image: 'assets/images/5cake.jpg',
+        category: 'dessert',
+        label: '',
+        price: '170',
+        description: 'A delectable, semi-sweet New York Style Cheese Cake, with cracker crust and spiced with Indian cardamoms',
+        comments: [
+            {
+                id: 0,
+                rating: 5,
+                comment: "Imagine all the eatables, living in conFusion!",
+                author: "Shanto",
+                date: "2018-10-16T17:57:28.556094Z"
+            },
+            {
+                id: 1,
+                rating: 4,
+                comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+                author: "Karim",
+                date: "2017-09-05T17:57:28.556094Z"
+            },
+            {
+                id: 2,
+                rating: 3,
+                comment: "Eat it, just eat it!",
+                author: "Moinul",
+                date: "2019-02-13T17:57:28.556094Z"
+            },
+            {
+                id: 3,
+                rating: 4,
+                comment: "Ultimate, Reaching for the stars!",
+                author: "Rahat",
+                date: "2017-12-02T17:57:28.556094Z"
+            },
+            {
+                id: 4,
+                rating: 2,
+                comment: "It's your birthday, we're gonna party!",
+                author: "Rony",
+                date: "2019-12-02T17:57:28.556094Z"
+            }
+        ]
+    },
+    {
+        id: 6,
+        name: 'Salad',
+        image: 'assets/images/6cake.jpg',
+        category: 'dessert',
+        label: '',
+        price: '170',
+        description: 'A delectable, semi-sweet New York Style Cheese Cake, with cracker crust and spiced with Indian cardamoms',
+        comments: [
+            {
+                id: 0,
+                rating: 5,
+                comment: "Imagine all the eatables, living in conFusion!",
+                author: "Shanto",
+                date: "2018-10-16T17:57:28.556094Z"
+            },
+            {
+                id: 1,
+                rating: 4,
+                comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+                author: "Karim",
+                date: "2017-09-05T17:57:28.556094Z"
+            },
+            {
+                id: 2,
+                rating: 3,
+                comment: "Eat it, just eat it!",
+                author: "Moinul",
+                date: "2019-02-13T17:57:28.556094Z"
+            },
+            {
+                id: 3,
+                rating: 4,
+                comment: "Ultimate, Reaching for the stars!",
+                author: "Rahat",
+                date: "2017-12-02T17:57:28.556094Z"
+            },
+            {
+                id: 4,
+                rating: 2,
+                comment: "It's your birthday, we're gonna party!",
+                author: "Rony",
+                date: "2019-12-02T17:57:28.556094Z"
+            }
+        ]
+    },
+    {
+        id: 7,
+        name: 'Chicken',
+        image: 'assets/images/7cake.jpg',
+        category: 'dessert',
+        label: '',
+        price: '170',
+        description: 'A delectable, semi-sweet New York Style Cheese Cake, with cracker crust and spiced with Indian cardamoms',
+        comments: [
+            {
+                id: 0,
+                rating: 5,
+                comment: "Imagine all the eatables, living in conFusion!",
+                author: "Shanto",
+                date: "2018-10-16T17:57:28.556094Z"
+            },
+            {
+                id: 1,
+                rating: 4,
+                comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+                author: "Karim",
+                date: "2017-09-05T17:57:28.556094Z"
+            },
+            {
+                id: 2,
+                rating: 3,
+                comment: "Eat it, just eat it!",
+                author: "Moinul",
+                date: "2019-02-13T17:57:28.556094Z"
+            },
+            {
+                id: 3,
+                rating: 4,
+                comment: "Ultimate, Reaching for the stars!",
+                author: "Rahat",
+                date: "2017-12-02T17:57:28.556094Z"
+            },
+            {
+                id: 4,
+                rating: 2,
+                comment: "It's your birthday, we're gonna party!",
+                author: "Rony",
+                date: "2019-12-02T17:57:28.556094Z"
+            }
+        ]
+    },
+    {
+        id: 8,
+        name: 'Masroom',
+        image: 'assets/images/8cake.jpg',
+        category: 'dessert',
+        label: '',
+        price: '170',
+        description: 'A delectable, semi-sweet New York Style Cheese Cake, with cracker crust and spiced with Indian cardamoms',
+        comments: [
+            {
+                id: 0,
+                rating: 5,
+                comment: "Imagine all the eatables, living in conFusion!",
+                author: "Shanto",
+                date: "2018-10-16T17:57:28.556094Z"
+            },
+            {
+                id: 1,
+                rating: 4,
+                comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+                author: "Karim",
+                date: "2017-09-05T17:57:28.556094Z"
+            },
+            {
+                id: 2,
+                rating: 3,
+                comment: "Eat it, just eat it!",
+                author: "Moinul",
+                date: "2019-02-13T17:57:28.556094Z"
+            },
+            {
+                id: 3,
+                rating: 4,
+                comment: "Ultimate, Reaching for the stars!",
+                author: "Rahat",
+                date: "2017-12-02T17:57:28.556094Z"
+            },
+            {
+                id: 4,
+                rating: 2,
+                comment: "It's your birthday, we're gonna party!",
+                author: "Rony",
+                date: "2019-12-02T17:57:28.556094Z"
+            }
+        ]
     }
 ]
 
